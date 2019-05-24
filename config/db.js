a818b19ad5060238a77ae8297166188e250e7da1
haney2121 <haney037332@gmail.com>
2019-05-24 18:29:40 -0500

    server setup

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+//allows me to use the default.json
+const config = require('config');
+//pulls my variables from the default.json
+const db = config.get('mongoURI');
+
+//Async function for starting and connecting to the DB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.log(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
